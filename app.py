f96d8469bc8709ed29709033a217035fd3181d92
douglasdcm <douglas.dcm@gmail.com>
2020-12-04 13:18:25 -0300

    adding flask port

--- app.py
+++ app.py
@@ -25,4 +25,5 @@ def worker():
 
 if __name__ == '__main__':
 	# run!
-	app.run(host='0.0.0.0', threaded=True)
+	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', threaded=True, port=port)
